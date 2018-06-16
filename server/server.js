89d5db38446dc4c50edf0532ad011d77351a21f6
gm019w <gm019w@att.com>
2018-06-17 02:02:00 +0530

    Added socket io events

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 const path = require('path');
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicpath =  path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
-const express = require('express');
-
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicpath));
 
- app.listen(port, () => {
+io.on('connection', (socket) => {
+
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client Disconnected');
+    });
+});
+
+
+ server.listen(port, () => {
 
         console.log(`Started on port: ${port}`);
  })
