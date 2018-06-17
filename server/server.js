bbe21621a075a100dd18e3138f477393855eb54d
gm019w <gm019w@att.com>
2018-06-17 23:40:29 +0530

    Greet new user and notify others

--- server/server.js
+++ server/server.js
@@ -16,7 +16,18 @@ io.on('connection', (socket) => {
 
     console.log('New user connected');
 
-    
+
+socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+});
+
+socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+})
 
 socket.on('createMessage', (newMessage) => {
 
@@ -26,7 +37,13 @@ socket.on('createMessage', (newMessage) => {
             from: newMessage.from,
             text: newMessage.text,
             createdAt: new Date().getTime()
-        })
+        });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: newMessage.from,
+        //     text: newMessage.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
