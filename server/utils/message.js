388c6907f881286ab5210a4699a9298d4baac1f5
gm019w <gm019w@att.com>
2018-06-23 01:57:25 +0530

    Added Timestamp formatting

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,10 +1,12 @@
+var moment = require('moment');
+
 var generateMessage = (from, text) => {
 
     return {
 
         from,
         text,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 };
 
@@ -14,7 +16,7 @@ var generateLocationMessage = (from, latitude, longitude) => {
 
         from,
         url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 };
 
