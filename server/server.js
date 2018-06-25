526455cc1742ef8cd426eb887645d25e1587e7d3
gm019w <gm019w@att.com>
2018-06-26 02:55:17 +0530

    Message sending to a room

--- server/server.js
+++ server/server.js
@@ -50,16 +50,27 @@ socket.on('join', (params, callback) => {
 
 socket.on('createMessage', (newMessage, callback) => {
 
-        console.log('createMessage', newMessage);
+        var user = users.getUser(socket.id);
 
-        io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+        if(user && isRealString(newMessage.text))
+        {
+            io.to(user.room).emit('newMessage', generateMessage(user.name, newMessage.text));
+        }
+        
         callback();
    
  });
 
  socket.on('createLocationMessage', (coords) => {
 
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+
+    if(user)
+    {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
+
+       
  });
 
     socket.on('disconnect', () => {
